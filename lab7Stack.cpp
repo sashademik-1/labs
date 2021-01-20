570e49d83f823cfc07b794cd0583863c82b75a62
sashademik-1 <70635435+sashademik-1@users.noreply.github.com>
2021-01-20 15:56:51 +0300

    Add files via upload

---
+++ lab7Stack.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+struct Node
+{
+    Node *pNext = nullptr;
+    int data = 0;
+} node;
+struct Stack
+{
+    Node *top;
+} stack;
+Stack &constructor(Stack &is)
+{
+    is.top = nullptr;
+    return is;
+}
+Stack &push(Stack &is, int data)
+{
+    Node *point = new Node;
+    point->data = data;
+    point->pNext = is.top;
+    is.top = point;
+    return is;
+};
+void print(const Stack &is)
+{
+    if (is.top == nullptr)
+    {
+        std::cout << "stack is empty" << std::endl;
+    }
+    else
+    {
+        for (auto show = is.top; show != nullptr; show = show->pNext)
+        {
+            std::cout << show->data << std::endl;
+        }
+    }
+}
+Stack &pop(Stack &is)
+
+{
+    if (is.top == nullptr)
+    {
+        std::cout << "stack is empty" << std::endl;
+    }
+    else
+    {
+        Node *point = is.top->pNext;
+        std::cout << is.top->data << std::endl;
+        delete is.top;
+        is.top = point;
+    }
+    return is;
+}
+Stack &distrucor(Stack &is)
+{
+    if (is.top == nullptr)
+    {
+        std::cout << "Stack is empty" << std::endl;
+    }
+    else
+    {
+        while (is.top != nullptr)
+        {
+            Node *point = is.top->pNext;
+            delete is.top;
+            is.top = point;
+        }
+    }
+    return is;
+}
+unsigned int size(const Stack &is)
+{
+    size_t length = 0;
+    for (auto iter = is.top; iter != nullptr; length++)
+    {
+        iter = iter->pNext;
+    }
+    std::cout << "Size: " << length << std::endl;
+}
+int main()
+
+{
+    Stack p;
+    constructor(p);
+    push(p, 8);
+    push(p, 9);
+    pop(p);
+    print(p);
+    size(p);
+    distrucor(p);
+    return 0;
+}
